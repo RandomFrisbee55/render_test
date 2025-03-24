0bf9cdacca459f5a72ccd602f5d3d8068b72da14
Stephanie Ryall <sryal027@uottawa.ca>
2025-03-24 10:34:32 -0400

    Update app.py
    with jsonify

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.form.to_dict()  # Get JotForm submission data
-    return f"Got your submission! You sent: {data}"
+    data = request.form.to_dict()
+    return jsonify({"webhookResponse": f"Got your submission! You sent: {data}"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
